75d19500c727902aa7919eb641c13083296a5da3
Andrea López Figueroa <lo.fig.andrea@gmail.com>
2024-09-17 12:22:02 +0200

    Added the code and the README.md

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,38 @@
 package com.example;
 
 public class Main {
+
+        // Basic arithmetic methods:
+        public static int add(int a, int b){
+            return a + b;
+        }
+    
+        public static int substract(int a, int b){
+            return a - b;
+        }
+    
+        public static int multiply(int a, int b){
+            return a * b;
+        }
+    
+        public static int divide(int a, int b){
+            return a / b;
+        }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 20;
+        int num2 = 5;
+
+        int additionResult = add(num1, num2);
+        System.out.println(num1 + " + " + num2 + " = " + additionResult);
+
+        int subtractionResult = substract(num1, num2);
+        System.out.println(num1 + " - " + num2 + " = " + subtractionResult);
+
+        int multiplicationResult = multiply(num1, num2);
+        System.out.println(num1 + " * " + num2 + " = " + multiplicationResult);
+
+        int divisionResult = divide(num1, num2);
+        System.out.println(num1 + " / " + num2 + " = " + divisionResult);
+    }
     }
 }
